db5bb68d8ba99ac3c74f6202002efebb9df0788c
Jacob Mendelowitz <jmdev37@gmail.com>
2017-09-19 22:35:01 -0400

    New UI for modal image view.

--- Snapshot/Snapshot/ModalImageViewController.swift
+++ Snapshot/Snapshot/ModalImageViewController.swift
@@ -8,16 +8,16 @@
 
 import UIKit
 
-class ModalImageViewController: UIViewController {
+class ModalImageViewController: UIViewController, UIScrollViewDelegate {
 
     // The photo to be displayed on the screen.
     @IBOutlet weak var photoView: UIImageView!
     
+    // The view that scrolls the photo and allows for zooming.
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     // The text below the photo displaying the expiration date.
     @IBOutlet weak var photoText: UILabel!
-    
-    // The white background behind the photo.
-    @IBOutlet weak var photoBackground: UIView!
     
     // The button to close the photo modal.
     @IBOutlet weak var closeButton: UIButton!
@@ -33,14 +33,23 @@ class ModalImageViewController: UIViewController {
         super.viewDidLoad()
         setupImage()
         setupActions()
+        setupScrollView()
+    }
+    
+    // Runs when the view controller is about to appear.
+    internal override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isStatusBarHidden = true
+    }
+    
+    // Runs when the view controller is about to disappear.
+    internal override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isStatusBarHidden = false
     }
     
     // Sets up the enlarged image view.
     private func setupImage() {
-        photoBackground.layer.cornerRadius = 10
-        photoBackground.clipsToBounds = true
-        closeButton.layer.cornerRadius = 10
-        closeButton.clipsToBounds = true
         if let savedImage = selectedImage {
             photoView.image = savedImage.image
             let dateFormatter = DateFormatter()
@@ -53,6 +62,62 @@ class ModalImageViewController: UIViewController {
     // Sets up the actions for any interactive objects on the screen.
     private func setupActions() {
         closeButton.addTarget(self, action: #selector(closeButtonPress(_:)), for: .touchDown)
+    }
+    
+    // Sets up the scroll view to manipulate the image on the screen.
+    private func setupScrollView() {
+        scrollView.delegate = self
+        scrollView.contentSize = view.frame.size
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 6.0
+    }
+    
+    // This is the view that will be zoomed in the scroll view.
+    internal func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoView
+    }
+    
+    // This runs when the user zooms the view.
+    internal func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        closeButton.isHidden = true
+        photoText.isHidden = true
+        repositionScrolledImage()
+    }
+    
+    // This runs when the user ends a zoom.
+    internal func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        if scrollView.zoomScale <= scrollView.minimumZoomScale {
+            closeButton.isHidden = false
+            photoText.isHidden = false
+        }
+    }
+    
+    
+    // This runs when the user finishes scrolling.
+    internal func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+
+    }
+    
+    // Keeps the image being viewed steady on the screen as the user zooms.
+    private func repositionScrolledImage() {
+        let imageViewSize = photoView.frame.size
+        let imageSize = photoView.image!.size
+        var realImageSize: CGSize
+        if (imageSize.width / imageSize.height) > (imageViewSize.width / imageViewSize.height) {
+            let heightTimesRatio = (imageViewSize.width / imageSize.width) * imageSize.height
+            realImageSize = CGSize(width: imageViewSize.width, height: heightTimesRatio)
+        } else {
+            let widthTimesRatio = (imageViewSize.height / imageSize.height) * imageSize.width
+            realImageSize = CGSize(width: widthTimesRatio, height: imageViewSize.height)
+        }
+        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        frame.size = realImageSize
+        photoView.frame = frame
+        
+        let screenSize = scrollView.frame.size
+        let ox = (screenSize.width > realImageSize.width) ? (screenSize.width - realImageSize.width) / 2 : 0
+        let oy = (screenSize.height > realImageSize.height) ? (screenSize.height - realImageSize.height) / 2 : 0
+        scrollView.contentInset = UIEdgeInsetsMake(oy, ox, oy, ox)
     }
     
     // Go back to the photo library when pressed.
